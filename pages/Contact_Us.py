7a7f30399152a8643c45b6dd911581effbfd6c37
Ethan <shihuidev@gmail.com>
2022-12-20 23:36:12 +0000

    added "Contact us" feature

---
+++ pages/Contact_Us.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from send_email import send_email
+
+st.header("Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    topic = st.selectbox(
+        "What topic do you want to discuss?",
+        ("Job Inquiries", "Project Proposals", "Other")
+    )
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+Topic: {topic}
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully!")
---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "l327575321@gmail.com"
+    password = os.getenv("PASSWORD")
+    context = ssl.create_default_context()
+    receiver = "l327575321@gmail.com"
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
